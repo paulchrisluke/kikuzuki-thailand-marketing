e5f07cca8967bef6c7931c46797ad3980f88447c
Joe Elia <44442621+joeelia@users.noreply.github.com>
2024-10-21 14:29:38 -0400

    remove nitro

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,15 +2,6 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  nitro: {
-    hooks: {
-      "prerender:generate"(route) {
-        if (route.route?.includes("private")) {
-          route.skip = true;
-        }
-      },
-    },
-  },
   css: ['~/assets/css/main.css'],
   postcss: {
     plugins: {
