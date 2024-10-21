91da93902784912069499fc44ffda0d70e83118e
Joe Elia <44442621+joeelia@users.noreply.github.com>
2024-10-21 17:18:50 -0400

    woek?

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,10 +3,10 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   nitro: {
-    static: true,
+    preset: 'static',
     prerender: {
       autoSubfolderIndex: false,
-      routes: ['/'] // Add root route for prerendering
+      routes: ['/', '/404'],
     }
   },
   css: ['~/assets/css/main.css'],
