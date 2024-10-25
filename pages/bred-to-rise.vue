fd10282c6f98feb52bdbb74076c04cff3ee737d6
Dom George <domg93@icloud.com>
2024-10-25 06:51:25 -0400

    bred2

--- pages/bred-to-rise.vue
+++ pages/bred-to-rise.vue
@@ -1,29 +1,35 @@
 <template>
-    <article class="mx-auto prose lg:prose-xl py-20 px-8">
-      <h1>Bred to Rise</h1>
+    <article>
+      <div class="bg-black text-white w-full py-20 px-8">
+        <div class="mx-auto prose lg:prose-xl">
+          <h1 class="text-white">Bred to Rise</h1>
 
-    <p> <strong> Bred to Rise</strong> is a give-back initiative rooted in Sand-O’s mission to uplift communities 
-        through mental wellness, nutrition, and personal development. In 2024, we will reward 
-        resilience—supporting those overcoming mental health challenges, pursuing fitness goals, 
-        and rebuilding their lives.
-    </p>
-    <p>  Our goal is to create meaningful moments of progress by providing tools, meals, and support that 
-        help individuals rise to their full potential. Just like bread, people rise best with the right care and attention.
-    </p>
+          <p class="text-white"> <strong class="text-white"> Bred to Rise</strong> is a give-back initiative rooted in Sand-O's mission to uplift communities 
+              through mental wellness, nutrition, and personal development. In 2024, we will reward 
+              resilience—supporting those overcoming mental health challenges, pursuing fitness goals, 
+              and rebuilding their lives.
+          </p>
+          <p class="text-white">  Our goal is to create meaningful moments of progress by providing tools, meals, and support that 
+              help individuals rise to their full potential. Just like bread, people rise best with the right care and attention.
+          </p>
+        </div>
+      </div>
 
-    <h2>Community Uprising Initiative</h2>
-    <p> At <strong> Bred to Rise</strong>, we focus on empowering local communities by providing the tools and resources 
-          needed for personal transformation. Whether it’s through healthy meals, fitness programs, or 
-          mental wellness workshops, we aim to uplift individuals facing challenges such as mental health 
-          struggles, addiction recovery, or life after incarceration.</p>
+      <div class="mx-auto prose lg:prose-xl px-8 py-20">
+        <h2>Community Uprising Initiative</h2>
+        <p> At <strong> Bred to Rise</strong>, we focus on empowering local communities by providing the tools and resources 
+              needed for personal transformation. Whether it's through healthy meals, fitness programs, or 
+              mental wellness workshops, we aim to uplift individuals facing challenges such as mental health 
+              struggles, addiction recovery, or life after incarceration.</p>
 
-         <p>Our community partners—including gyms, bakeries, schools, and wellness centers—play a vital role 
+        <p>Our community partners—including gyms, bakeries, schools, and wellness centers—play a vital role 
           in delivering our programs. Together, we build environments where people can rise stronger, one step at a time. 
           To connect with a Bred to Rise program or explore ways to support our efforts, click below.</p> 
+        <div class="mt-12 mb-8">
           <button class="bg-black text-white px-8 py-4 rounded-full text-lg font-bold hover:bg-gray-300">
-          Inquire
-        </button>
-
-  
-</article>
+            Inquire
+          </button>
+        </div>
+      </div>
+    </article>
 </template>
