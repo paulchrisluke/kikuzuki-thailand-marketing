d6367755065d00f8b92ab5107c87c0fceb5ae5c8
Paul Chris Luke <paulchrisluke@gmail.com>
2026-05-03 14:45:17 +0700

    feat(auth): add Google Search Console and Analytics readonly scopes

--- server/api/auth/google.get.ts
+++ server/api/auth/google.get.ts
@@ -14,7 +14,14 @@ export default defineEventHandler(async (event) => {
   authUrl.searchParams.set('client_id', env.GOOGLE_CLIENT_ID)
   authUrl.searchParams.set('redirect_uri', redirectUri)
   authUrl.searchParams.set('response_type', 'code')
-  authUrl.searchParams.set('scope', 'openid email profile https://www.googleapis.com/auth/business.manage')
+  authUrl.searchParams.set('scope', [
+    'openid',
+    'email', 
+    'profile',
+    'https://www.googleapis.com/auth/business.manage',
+    'https://www.googleapis.com/auth/webmasters.readonly',
+    'https://www.googleapis.com/auth/analytics.readonly'
+  ].join(' '))
   authUrl.searchParams.set('state', state)
   authUrl.searchParams.set('access_type', 'offline')
   authUrl.searchParams.set('prompt', 'consent select_account')
