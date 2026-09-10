adcb5fd4ec31657e64b44ffe9d7a2cf57b74fb66
Paul Chris Luke <paulchrisluke@users.noreply.github.com>
2026-09-10 17:09:45 +0700

    fix(fonts): use Node Buffer types in the build-time asset module

--- modules/mali-fonts.ts
+++ modules/mali-fonts.ts
@@ -1,3 +1,4 @@
+import { Buffer } from 'node:buffer'
 import { mkdir, readFile, rename, writeFile } from 'node:fs/promises'
 import { resolve } from 'node:path'
 import { randomUUID } from 'node:crypto'
